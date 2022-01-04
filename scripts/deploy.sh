3083e1976512d13f05032e5878bda44c4de224ec
mathink12 <showmethecode@foxmail.com>
2022-01-04 20:54:15 +0800

    init

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 进入生成的文件夹
+cd assets
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mathink12/genshin-impact-assets.git master:gh-pages
+
+cd -
